6d4bd1390a2d20148c116240f7e8a241bddeee17
Fabrice Madre <stepwiseltd@gmail.com>
2021-07-30 16:12:59 +0400

    styling webapp

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  has_many :lists, through: :bookmarks
   validates :title, presence: true
   validates :overview, presence: true
   validates :title, uniqueness: true
